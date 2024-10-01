58af7ef3765eadaefffa7435a5beb551e199e849
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-10-01 15:22:09 +0530

    pkNavratri

---
+++ js/script.js
@@ -0,0 +1,434 @@
+function infofunc01(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo1");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo0");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+
+function infofunc23(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo3");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo2");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+// function infofunc45(e, info) {
+//    var i, contentsr1, linkssr1;
+//    contentsr1 = document.getElementsByClassName("demo5");
+//    for (i = 0; i < contentsr1.length; i++) {
+//       contentsr1[i].style.display = "none";
+//    }
+//    linkssr1 = document.getElementsByClassName("demo4");
+//    for (i = 0; i < linkssr1.length; i++) {
+//       linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+//    }
+//    document.getElementById(info).style.display = "block";
+//    e.currentTarget.className += " active";
+// }
+// function infofunc67(e, info) {
+//    var i, contentsr1, linkssr1;
+//    contentsr1 = document.getElementsByClassName("demo7");
+//    for (i = 0; i < contentsr1.length; i++) {
+//       contentsr1[i].style.display = "none";
+//    }
+//    linkssr1 = document.getElementsByClassName("demo6");
+//    for (i = 0; i < linkssr1.length; i++) {
+//       linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+//    }
+//    document.getElementById(info).style.display = "block";
+//    e.currentTarget.className += " active";
+// }
+// function infofunc89(e, info) {
+//    var i, contentsr1, linkssr1;
+//    contentsr1 = document.getElementsByClassName("demo9");
+//    for (i = 0; i < contentsr1.length; i++) {
+//       contentsr1[i].style.display = "none";
+//    }
+//    linkssr1 = document.getElementsByClassName("demo8");
+//    for (i = 0; i < linkssr1.length; i++) {
+//       linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+//    }
+//    document.getElementById(info).style.display = "block";
+//    e.currentTarget.className += " active";
+// }
+function infofunc001(e, info, info1) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo01");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo00");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   document.getElementById(info1).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function infofunc023(e, info, info1) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo03");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo02");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   document.getElementById(info1).style.display = "block";
+   e.currentTarget.className += " active";
+}
+
+function tinfofunc23(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo3");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo2");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc45(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo5");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo4");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc67(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo7");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo6");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc89(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo9");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo8");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc001(e, info, info1) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo01");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo00");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   document.getElementById(info1).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc023(e, info, info1) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo03");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo02");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   document.getElementById(info1).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function infofunc045(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo05");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo04");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function infofunc067(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo07");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo06");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function infofunc089(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo09");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo08");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc045(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo05");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo04");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc067(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo07");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo06");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function tinfofunc089(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("tdemo09");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("tdemo08");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function infofunc0001(e, info) {
+   var i, contentsr1, linkssr1;
+   contentsr1 = document.getElementsByClassName("demo001");
+   for (i = 0; i < contentsr1.length; i++) {
+      contentsr1[i].style.display = "none";
+   }
+   linkssr1 = document.getElementsByClassName("demo000");
+   for (i = 0; i < linkssr1.length; i++) {
+      linkssr1[i].className = linkssr1[i].className.replace(" active", "");
+   }
+   document.getElementById(info).style.display = "block";
+   e.currentTarget.className += " active";
+}
+function showGuidline() {
+   document.getElementById("guidline").style.display = 'block';
+   document.getElementById("main").classList.add('main');
+}
+function hideGuidline() {
+   console.log('called')
+   document.getElementById("guidline").style.display = 'none';
+   document.getElementById("main").classList.remove('main');
+}
+const s1btn = document.getElementsByClassName('demo00');
+let s1SliderNumber = 0;
+
+function slider001(arrow) {
+   if (arrow == "left" && s1SliderNumber > 0) {
+      s1SliderNumber = s1SliderNumber - 1;
+
+   }
+   else if (arrow == "left" && s1SliderNumber == 0) {
+      s1SliderNumber = 4;
+   }
+   else if (arrow == "right" && s1SliderNumber < 4) {
+      s1SliderNumber = s1SliderNumber + 1;
+   }
+   else {
+      s1SliderNumber = 0;
+   }
+   s1btn[s1SliderNumber].click();
+}
+
+const s2btn = document.getElementsByClassName('demo02');
+let s2SliderNumber = 0;
+
+function slider023(arrow) {
+   if (arrow == "left" && s2SliderNumber > 0) {
+      s2SliderNumber = s2SliderNumber - 1;
+
+   }
+   else if (arrow == "left" && s2SliderNumber == 0) {
+      s2SliderNumber = 4;
+   }
+   else if (arrow == "right" && s2SliderNumber < 4) {
+      s2SliderNumber = s2SliderNumber + 1;
+   }
+   else {
+      s2SliderNumber = 0;
+   }
+   s2btn[s2SliderNumber].click();
+}
+
+const ts1btn = document.getElementsByClassName('tdemo00');
+let ts1SliderNumber = 0;
+
+function tslider001(arrow) {
+   if (arrow == "left" && ts1SliderNumber > 0) {
+      ts1SliderNumber = ts1SliderNumber - 1;
+
+   }
+   else if (arrow == "left" && ts1SliderNumber == 0) {
+      ts1SliderNumber = 3;
+   }
+   else if (arrow == "right" && ts1SliderNumber < 3) {
+      ts1SliderNumber = ts1SliderNumber + 1;
+   }
+   else {
+      ts1SliderNumber = 0;
+   }
+   ts1btn[ts1SliderNumber].click();
+}
+const ts2btn = document.getElementsByClassName('tdemo02');
+let ts2SliderNumber = 0;
+
+function tslider023(arrow) {
+   if (arrow == "left" && ts2SliderNumber > 0) {
+      ts2SliderNumber = ts2SliderNumber - 1;
+
+   }
+   else if (arrow == "left" && ts2SliderNumber == 0) {
+      ts2SliderNumber = 3;
+   }
+   else if (arrow == "right" && ts2SliderNumber < 3) {
+      ts2SliderNumber = ts2SliderNumber + 1;
+   }
+   else {
+      ts2SliderNumber = 0;
+   }
+   ts2btn[ts2SliderNumber].click();
+}
+
+setInterval(() => {
+   slider001('right');
+   slider023('right');
+   tslider001('right');
+   tslider023('right');
+}, 3000);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+   document.getElementById("mybutton01").click();
+   document.getElementById("mybutton23").click();
+   // document.getElementById("mybutton45").click();
+   // document.getElementById("mybutton67").click();
+   // document.getElementById("mybutton89").click();
+   document.getElementById("mybutton001").click();
+   document.getElementById("mybutton023").click();
+   document.getElementById("tmybutton23").click();
+   document.getElementById("tmybutton45").click();
+   document.getElementById("tmybutton67").click();
+   document.getElementById("tmybutton89").click();
+   document.getElementById("tmybutton001").click();
+   document.getElementById("tmybutton023").click();
+   document.getElementById("mybutton045").click();
+   // document.getElementById("mybutton067").click();
+   // document.getElementById("mybutton089").click();
+   document.getElementById("tmybutton045").click();
+   document.getElementById("tmybutton067").click();
+   document.getElementById("tmybutton089").click();
+   document.getElementById("mybutton0001").click();
+});
+
+
+function get_user_img(u_id) {
+   $.ajax({
+      type: 'GET',
+      async: true,
+      url: 'http://www.streamkarlive.live/meShow/entrance?parameter=%7B%22FuncTag%22:10005044,%22userId%22:' + u_id + '%7D',
+      cache: false,
+      error: function (xhr, ajaxOptions, thrownError) {
+         // alert(xhr.responseText);
+         //   alert(thrownError);
+      },
+      xhr: function () {
+         var xhr = new window.XMLHttpRequest();
+
+         //Download progress
+         xhr.addEventListener("progress", function (evt) {
+            console.log(evt.lengthComputable);
+            if (evt.lengthComputable) {
+               console.log("Downloading contents.");
+            }
+         }, false);
+         return xhr;
+      },
+      beforeSend: function () {
+         // $('#img_bulk_anim_1').attr('src', 'img/bx_loader.gif');
+         //$('#div_contents').html('<img id="img_wait" src="images/bx_loader.gif"/>');
+      },
+      complete: function () {
+         // $('#img_bulk_anim_1').removeAttr('src');
+      },
+      success: function (data) {
+
+         user_data = jQuery.parseJSON(data);
+
+
+         $('#u_img_' + u_id + '').attr("src", '' + user_data.portrait_path_original + '!256');
+
+
+         console.log('Ajax success');
+      },
+      failure: function () {
+         console.log('Ajax Failure.');
+      }
+
+   });
+
+
+
+}
+
